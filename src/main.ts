d2aa9701cc59468fd07500cda4cb0eb8d47f9a46
diogofrr <ddiogof20@gmail.com>
2024-05-31 15:53:18 -0300

    feat: add swagger

--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,5 @@
 import { NestFactory } from '@nestjs/core';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
 import * as dotenv from 'dotenv';
@@ -6,6 +7,15 @@ import * as dotenv from 'dotenv';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   dotenv.config();
+
+  const config = new DocumentBuilder()
+    .setTitle('Fretes API')
+    .setDescription('Descrição da API de fretes.')
+    .setVersion('1.0')
+    .build();
+
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
 
   app.useGlobalPipes(
     new ValidationPipe({
